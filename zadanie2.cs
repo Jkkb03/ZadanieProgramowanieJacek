1125a4b54494f31537d7d1b902fe844d75c697f3
Jkkb03 <jacekkabot28@gmail.com>
2025-05-31 12:24:32 +0200

    zadanie2: Rozwiązanie zadanie nr 2

---
+++ zadanie2.cs
@@ -0,0 +1,12 @@
+// Write a C# Sharp program to print the result of dividing two numbers.
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int Liczba1 = 10;
+        int Liczba2 = 2;
+        Console.WriteLine("Result of division: " + (Liczba1 / Liczba2));
+    }
+}
